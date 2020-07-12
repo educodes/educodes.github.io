e7ecbc8a3d9d724f2f06917206a3b8d6eae3877e
educodes <educodes0@gmail.com>
2020-07-12 12:05:26 -0500

    initial commit website

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("show");
+});
+
+// queryselector --> selecciona un elemento
+
+ScrollReveal().reveal(".about-me", { delay: 600 });
+ScrollReveal().reveal(".skills", { delay: 600 });
+ScrollReveal().reveal(".study", { delay: 600 });
+// ScrollReveal().reveal(".card-banner-two", { delay: 500 });
